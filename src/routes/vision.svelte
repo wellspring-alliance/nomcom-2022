cdc6c3b71098b2414ddf6355d9314b4a5130884f
Ken Kunz <kennethkunz@gmail.com>
2022-03-30 13:36:26 -0500

    add vision, context, commitments

--- src/routes/vision.svelte
+++ src/routes/vision.svelte
@@ -1,3 +1,97 @@
 <h1>Vision</h1>
 
-<p>Check back soon for more info about the 2022 Nominating Committee's vision.</p>
+<h2>Our Goal</h2>
+<p>
+  The Wellspring Nominating Committee exists for a <strong>limited season</strong>
+  to identify the most <strong>qualified candidates</strong> to fill Wellspring’s
+  elected leadership positions and to <strong>clearly communicate</strong> that slate
+  of nominees to our church body.
+</p>
+<ul>
+  <li>
+    <strong>Limited Season:</strong> Unlike standing committees, the Nominating
+    Committee is a “popup committee” – new committee is elected / appointed annually,
+    begins work in January, finishes shortly after the annual member meeting in June.
+  </li>
+  <li>
+    <strong>Qualified Candidates:</strong> both individually and collectively…
+    <ul>
+      <li>
+        <em>individually</em> – character (spiritual maturity), competencies (spiritual
+        gifts, strengths, experience), and commitment (to a path forward for Wellspring;
+        to the responsibilities of the role)
+      </li>
+      <li>
+        <em>collectively</em> – diversity (across many dimensions); ability to navigate
+        conflict
+      </li>
+    </ul>
+  </li>
+  <li>
+    <strong>Clearly Communicate:</strong> what are the roles, who are the nominees, why
+    we believe they are the right people for the roles at this time.
+  </li>
+</ul>
+
+<h2>Our Context</h2>
+<p>
+  Wellspring is in a season of crisis. The climate within our church body is one of
+  uncertainty about the future and decreased confidence in leadership. Meanwhile, our
+  leaders feel exhausted by the increased demands on their time and emotional energy
+  and the increased scrutiny of their decisions and motives.
+</p>
+<p>
+  This makes our work as a committee both <em>harder</em> and <em>more important</em>.
+  <em>Harder</em> to find qualified leaders who are willing to sign up for leadership
+  or continue to serve in this context. <em>More important</em> because the stakes are
+  higher – we must begin to rebuild trust in order to move forward as a community.
+</p>
+<p>
+  This context must inform our work – including how we work together as a team, how
+  we involve the congregation, who we seek to nominate, and how we communicate with
+  the body throughout the process. And above all… how we depend on God and seek Him
+  throughout this process – the God of all grace and truth who redeems all things.
+</p>
+
+<h2>Our Commitments</h2>
+<ul>
+  <li>
+    We will strive to create a <strong>culture of respect, charity and trust</strong>
+    with one another and others.
+  </li>
+  <li>
+    We will seek <strong>genuine unity rather than simple majority rule</strong> in
+    our shared decisions.
+  </li>
+  <li>
+    We will value <strong>thoroughness over efficiency</strong> in our discernment process.
+  </li>
+  <li>
+    Will will strive to <strong>balance transparency and confidentiality</strong> with one
+    another and with the body.
+  </li>
+  <li>
+    We will <strong>represent and uphold C&MA Beliefs</strong> in our process and in the
+    nominees we identify.
+  </li>
+</ul>
+
+<h3>What do we mean by <em>balance transparency and confidentiality?</em></h3>
+<ul>
+  <li>
+    We will be <em>transparent whenever possible</em> regarding our process, decisions
+    and interactions.
+  </li>
+  <li>
+    We will <em>hold things in confidence</em> when necessitated by the sensitive nature
+    of a topic or to respect the wishes of those involved.
+  </li>
+  <li>
+    We will strive to be clear about whether confidentiality is required in a given
+    situation; when in doubt, we will assume confidentiality until we confirm otherwise.
+  </li>
+  <li>
+    We will not arbitrarily invoke confidentiality as a shield to transparency.
+  </li>
+</ul>
+
