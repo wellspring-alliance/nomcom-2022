8aa8bf44e6a29efd97f2052e095b4fb6a997fbba
Ken Kunz <kennethkunz@gmail.com>
2022-05-04 09:20:23 -0500

    add May 1 2022 update to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,20 +10,66 @@
 </p>
 
 <p>
-  Please complete this brief survey to help us identify potential candidates. You can learn more
-  about the positions we are looking to fill <a href="/positions">here</a>.
+  <strong><em>Our Leaders Survey ended on April 9, 2022.</em></strong>
+  See update below.
 </p>
 
 <p>
-  <a class="btn btn-primary" href="/survey">Begin Survey</a>
-</p>
-
-<p>
-  If there are additional thoughts you'd like to share with the committee, please reach out to
-  one of the individuals below or drop us an email at
+  If there are any thoughts you'd like to share with the committee, please reach out to us at:
   <a href="mailto:nomcom@wellspringalliance.net">nomcom@wellspringalliance.net</a>.
 </p>
 
+<h2>
+  Update: May 1, 2022
+</h2>
+
+<p>
+  A few weeks ago the Nominating Committee asked for your
+  input on potential leaders within our body. <strong>35 of you</strong>
+  shared <strong>115 recommendations</strong> of <strong>78 different
+  people</strong>. This is far more input than we’ve received in recent
+  years. So, those of you who took a few minutes to share your input…
+  <em>thank you!</em> We are really grateful and encouraged by this.
+  It demonstrates that you as a body are engaged, that you care deeply
+  about Wellspring’s future. It also highlights what a gifted congregation
+  we have.
+</p>
+
+<p>
+  And… this presents a challenge for the nominating committee. We started
+  this process later than usual (due to timing of the CHC report) and
+  received far more input than usual. Our committee has finite resources and
+  time – practically, there’s no way we could reach out to everyone who was
+  raised to our attention. So, we’ve had to prioritize our outreach based on
+  a variety of factors, including: the number of recommendations someone
+  received, the content of the recommendations, past conversations with the
+  Committee and other factors.
+</p>
+
+<p>
+  We want to remind you that a <em>recommendation</em> to the committee is
+  not the same as a <em>nomination</em> for an elected position. A
+  recommendation puts someone on our radar. A nomination requires a lot of
+  additional conversations, prayer and discernment, as well as willingness
+  to serve on the part of the nominee. If you were recommended to the
+  committee and you haven’t heard from us, don’t be discouraged! 1
+  Corinthians 12 reminds us that we all need each other. We all have
+  spiritual gifts. We’re all called to serve. Only a few of you will be
+  called upon to serve in an elected leader position at this time. If that’s
+  not you, your gifts and service are no less important to this body. We
+  encourage you to be prayerful and faithful about how you are called to
+  serve in this season.
+</p>
+
+<p>
+  Over the next couple weeks, the committee will be very engaged in
+  conversations, prayer and discernment around potential nominees. And
+  candidates will also be in prayer and discernment. Towards the end of May,
+  we will finalize our nominations and communicate them to the congregation.
+  We’ll then provide some opportunities to get to know the candidates before
+  our June election. Please pray for the Nominating Committee during these
+  crucial next few weeks!
+</p>
 
 <p>
   Thank you,<br/><br/>
