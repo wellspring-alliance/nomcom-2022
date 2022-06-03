3ec01840785664f4d714fb39b83e942d9cf599d3
Ken Kunz <kennethkunz@gmail.com>
2022-06-03 17:30:28 -0500

    add first couple of FAQ questions/answers

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,5 +1,50 @@
-<h1>FAQ</h1>
+<h1>Frequently Asked Questions</h1>
+
+<h3>
+  How did the unique season we are in as a church impact the Nominating Committee’s work?
+</h3>
+<p>
+  We began our work with a strong sense that we would need to approach our work differently this
+  year. One of our first tasks as a team was to discuss our shared <em>vision, intentions and
+  means</em> in relation to our nomination work. The output of that discussion was a statement of
+  <a href="/vision">vision, context and commitments</a> that has guided us in our work.
+</p>
+
+<h3>
+  How did the committee identify potential candidates for various roles?
+</h3>
+<p>
+  In past years, the Nominating Committee has asked the congregation to share recommendations and
+  input on potential candidates for elected leadership positions. Unfortunately, the amount of input
+  the committee has received in recent years had been limited. We hoped that if we made a stronger
+  “call” and push for recommendations the congregation would respond. <em>You did!</em>
+</p>
 
 <p>
-  This page will contain the FAQ.
+  A few weeks ago the Nominating Committee asked for your input on potential leaders within our
+  body. <em>35 of you</em> shared <em>115 recommendations</em> of <em>78 different people</em>.
+  This is far more input than we’ve received in recent years. So, those of you who took a few
+  minutes to share your input… <em>thank you!</em> We are really grateful and encouraged by this.
+  It demonstrates that you as a body are engaged, that you care deeply about Wellspring’s future. It
+  also highlights what a gifted congregation we have. In addition, members of this committee pooled
+  their collective knowledge of our congregation to suggest prospective candidates.
 </p>
+
+<h3>
+  How did you narrow the candidates down to select nominees?
+</h3>
+<p>
+  TBD…
+</p>
+
+
+<style>
+  h3 {
+    margin-top: 2em;
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-top: 0.5em;
+  }
+</style>
