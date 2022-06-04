0c16c56ff1b8d60f668e7f208fd8012e5b61e60b
Ken Kunz <kennethkunz@gmail.com>
2022-06-04 01:23:17 -0500

    add Jane Larson bio

--- src/lib/data/index.js
+++ src/lib/data/index.js
@@ -4,6 +4,7 @@ export { default as mark_yarhouse } from './mark_yarhouse';
 
 // Members at Large
 export { default as eunju_yoon } from './eunju_yoon';
+export { default as jane_larson } from './jane_larson';
 export { default as philip_han } from './philip_han';
 
 // Deacons
---
+++ src/lib/data/jane_larson.js
@@ -0,0 +1,35 @@
+export default {
+
+name: 'Jane Larson',
+path: 'jane_larson',
+role: 'Member at Large',
+
+questions: [
+
+// How long have you attended Wellspring?
+`My family and I have attended Wellspring/Blanchard for 26 years.`,
+
+//  What are your spiritual gifts and how have you served in churches in the past?
+`My spiritual gifts are hospitality, administration, and leadership. Currently I
+am serving on the Personnel Committee and the Missions Committee at Wellspring.
+In the past I have served as a Home Group Leader, Co-Chair of the Pastor Search
+Committee, Governing Board At-Large Member (over 5 years ago), mentored college
+students, 2 short-term mission trips, children’s ministry, worship team, and
+choir.`,
+
+// How do you hope to serve in this season?
+`I would like to serve the people of Wellspring by listening and building
+increased transparency and trust.  I want to serve our church to encounter Jesus
+more intimately as we heal, build community, and develop a church that embraces
+and enjoys the strengths of diversity.`,
+
+//  What else would you like your Wellspring family to know about you?
+`I have had a career in business and have had the privilege of leading teams and
+bringing strategic thinking to problem solve.  I loved my work and have now
+recently retired. My husband and I have one adult daughter who is married and
+who was baptized at Blanchard. We live in Naperville. We are grateful to call
+Wellspring our church family and home.`
+
+]
+
+};
