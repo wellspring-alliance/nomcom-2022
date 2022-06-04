eec0392bd27651d81c6a4f8c1872c23b53447b78
Ken Kunz <kennethkunz@gmail.com>
2022-06-04 01:06:07 -0500

    add Eunju Yoon bio

---
+++ src/lib/data/eunju_yoon.js
@@ -0,0 +1,58 @@
+export default {
+
+name: 'Eunju Yoon',
+path: 'eunju_yoon',
+role: 'Member at Large',
+
+questions: [
+
+// How long have you attended Wellspring?
+`I have attended Wellspring since 2018.`,
+
+//  What are your spiritual gifts and how have you served in churches in the past?
+`Prayer and discipleship. Although prayer may feel like slow and ineffective, I
+have experienced that prayer is the surest and fastest way to have God’s will be
+done in my life and church through discernment of His heart and surrender to it.
+I also believe I’m called to help individuals deeply rooted in Christ and grow
+as reproducing disciples.
+<br /><br />
+As for visible services in church, I’ve served as a deacon (2019-2021) and home
+group leader for Korean speaking congregants (2019-present). God gave me a
+motherly heart for Wellspring. This church is so precious for numerous reasons,
+so prayer for Wellspring is my invisible work, joy, and rewards.
+`,
+
+// How do you hope to serve in this season?
+`Wellspring is very special and precious -- a church that doesn’t shy away from
+wrestling with difficult topics or confine God within our comfort zone. As a
+church that strives to follow the immensity of God in the broad gamut of pure
+gospel, work of Spirit, international work, social justice, etc., I believe God
+has very special mission and vision for Wellspring. Our church is blessed and
+privileged with so many resources and gifts, which makes us responsible for
+local/international and believing/nonbelieving communities beyond Wheaton.
+<br /><br />
+Additionally, it appears “social justice” has replaced the position of “God” as
+the ultimate goodness while U.S. society enters the era of post-Christianity.
+Therefore, Wellspring as a truly multicultural/multiethnic church (i.e., a
+microcosm of heaven) would give important messages to both church communities
+and nonbelieving society.
+<br /><br />
+I see hope in Wednesday prayer meetings when individuals with different
+thoughts, positions, etc. gather and pray together in one Spirit. I experience
+the healing power of oneness when we pray together, which we need to fulfill the
+God given mission and vision for Wellspring. Additionally, I hope my background
+as a counseling/cultural psychologist would help church recover from the wounds,
+navigate cultural/racial dynamics as a multicultural/multiethnic church, and
+have God’s vision realized within and through our church.
+`,
+
+//  What else would you like your Wellspring family to know about you?
+`I’m blessed with a husband, two adult children, their spouses, and four
+grandkids. I grew up as a child of a Baptist pastor and accepted Jesus as my
+savior and lord at the age of 18. My family immigrated from S. Korea to the U.S.
+24 yeas ago. I currently work as a professor in Counseling Psychology program at
+Loyola University Chicago.`
+
+]
+
+};
--- src/lib/data/index.js
+++ src/lib/data/index.js
@@ -1,3 +1,4 @@
 export { default as anne_sullivan } from './anne_sullivan';
+export { default as eunju_yoon } from './eunju_yoon';
 export { default as joe_schlie } from './joe_schlie';
 export { default as philip_han } from './philip_han';
