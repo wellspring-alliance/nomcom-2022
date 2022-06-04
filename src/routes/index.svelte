4e4bc0899448df6fedcafe690acd04007d0974f2
Ken Kunz <kennethkunz@gmail.com>
2022-06-04 14:43:11 -0500

    update home page with latest update!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,16 +19,69 @@
 </h2>
 
 <p>
-  New update copy!
-</p>
-
-
-<p>
-  Thank you,<br/><br/>
-  Wellspring Nominating Committee<br>
-  Felicia Bullock, Grace Chan, James Cuénod, Philip Han, Young Ok Kim, Ken Kunz
+  Dear Wellspring Community,
 </p>
 
 <p>
-  <a href="update-20220501">Update: May 1, 2022</a>
+I apologize for the delay in sending out this communication. The Nominating
+Committee recognizes the importance of transparency and timely communication,
+and had hoped to share this information with you sooner. <i>We’re sorry!</i>
+
+<p>
+It’s a joy and honor to finally share the nominations with you now. This has
+truly been a “labor of love” by the Nominating Committee, representing over 400
+hours of conversations, meetings and prayer.
+
+<ul>
+  <li>
+    The list of nominees can be viewed online <a href="/nominees">here</a>. Click
+    on any of the nominee photos to read the nominee’s bio.
+  </li>
+
+  <li>
+    We also welcome you to read the <a href="/vision">vision, context and commitments</a>
+    that have guided us in our work.
+  </li>
+
+  <li>
+    Finally, we have prepared an <a href="/faq">FAQ</a> that answers some questions we
+    received or anticipated.
+  </li>
+</ul>
+
+<p>
+For members, additional information about voting at the annual meeting will be
+provided next week. As a reminder, please send any floor nominations ahead of
+time so we have time to include them in the electronic ballot. In particular, we
+want to invite nominations for next year’s Nominating Committee. If you know
+someone who is spiritually mature, enjoys talking to people, and cares deeply
+about God’s work at Wellspring, please consider nominating them. For all floor
+nominations, please confirm the nominee is willing to serve in the role, and
+email their name and the role to nomcom@wellspringalliance.net by June 10th.
+</p>
+
+<p>
+Thank you for your prayers and for the trust you have placed in the Nominating
+Committee in this important season.
+</p>
+
+<p>
+Grace and peace,<br/><br/>
+Wellspring Nominating Committee<br/>
+Felicia Bullock, Grace Chan, James Cuénod, Philip Han, Young Ok Kim, Ken Kunz (chair)<br />
+Pastor Mitch Kim (ex officio)
+</p>
+
+<h2>
+  Previous Updates
+</h2>
+
+<!-- 
+<p>
+  <a href="update-20220529">May 29, 2022</a>
+</p>
+-->
+
+ <p>
+  <a href="update-20220501">May 1, 2022</a>
 </p>
