d34e129be6a8a5d6cd9253ebbc17aa9466461e37
Ken Kunz <kennethkunz@gmail.com>
2022-06-04 12:18:00 -0500

    change GB header; refactor

--- src/routes/nominees/index.svelte
+++ src/routes/nominees/index.svelte
@@ -3,6 +3,15 @@
   import * as importedBios from '$lib/data';
 
   const bios = Object.values(importedBios).sort((a, b) => a.name > b.name ? 1 : -1);
+
+  const elders = bios.filter((bio) => bio.role === 'Elder');
+
+  const govBoard = bios.filter((bio) => {
+    return ['Member at Large'].includes(bio.role);
+  });
+
+  const deacons = bios.filter((bio) => bio.role === 'Deacon');
+
 </script>
 
 <h1>Nominees</h1>
@@ -12,10 +21,10 @@
 </p>
 
 <h2>Elder Nominees</h2>
-<NomineeList bios={bios.filter((bio) => bio.role === 'Elder')} />
+<NomineeList bios={elders} />
 
-<h2>Member at Large Nominees</h2>
-<NomineeList bios={bios.filter((bio) => bio.role === 'Member at Large')} />
+<h2>Governing Board Nominees</h2>
+<NomineeList bios={govBoard} />
 
 <h2>Deacon Nominees</h2>
-<NomineeList bios={bios.filter((bio) => bio.role === 'Deacon')} />
+<NomineeList bios={deacons} />
