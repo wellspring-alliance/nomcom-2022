74d1c478672c38a3e877c4d953bcffe9c2215517
Ken Kunz <kennethkunz@gmail.com>
2022-06-04 14:59:58 -0500

    update FAQ

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -34,9 +34,143 @@
   How did you narrow the candidates down to select nominees?
 </h3>
 <p>
-  TBD…
+  Our narrowing “funnel” had a few steps:
+</p>
+<ol>
+  <li>
+    Initial suggestions from congregation and committee
+  </li>
+  <li>
+    Primary inquiries
+    <ol type="a">
+      <li>
+        Prioritised based on a number of factors, including the number and
+        content of recommendations someone received, past conversations with the
+        committee and other factors
+      </li>
+      <li>
+        Questions around gifting, service, and openness to being candidate
+      </li>
+    </ol>
+  </li>
+  <li>
+    Secondary inquiries
+    <ol type="a">
+      <li>
+        Questions around leadership and church needs in this season, and how the
+        candidates see alignment between those needs and their own gifting
+      </li>
+      <li>
+        Email references from two individuals
+      </li>
+      <li>
+        Followup conversations with committee members where further questions or
+        concerns were raised
+      </li>
+    </ol>
+  </li>
+</ol>
+<p>
+  Between and during each of these steps, we as a committee met for countless
+  hours to discuss and wrestle and discern – per our commitments, we aimed for
+  thoroughness and a sense of unity out of our debate, rather than efficiency or
+  rushing to a vote.
 </p>
 
+<h3>
+  Why did you nominate only one candidate per open role?
+</h3>
+<p>
+  As a committee, we wrestled with this question both individually and as a group.
+  While there are benefits to nominating multiple candidates per opening, we
+  ultimately decided collectively that nominating one candidate per role reflects
+  the time and discernment we have put into the process and helps direct us as a
+  church towards unity rather than further fracturing. We also want to affirm the
+  choice and agency on the part of every member in the voting process, both in the
+  2/3 majority needed to confirm a nominee and in the opportunity for members to
+  nominate from the floor.
+</p>
+
+<h3>
+  Why wasn’t [fill in the blank] nominated?
+</h3>
+<p>
+  In balancing confidentiality and transparency, we are unable to answer specifics
+  as to why any particular candidate was not nominated. We as a committee sought
+  unity - not uniformity - so we reached decisions only after much discussion and
+  often much disagreement. In the reality of disagreement and differing views, we
+  also committed to listening to each other and accepting the group’s collective
+  wisdom in decisions. We recognize that we as a church body are in a season of
+  rebuilding trust, and hope that the many facets of diversity represented within
+  this committee, the way we have sought thoroughness and unity amidst
+  differences, and our commitments to transparency and clear communication will
+  together form a foundation of trustworthiness both for our committee and our
+  approach.
+</p>
+
+<h3>
+  How did you address potential conflicts of interest?
+</h3>
+<p>
+  Although some of our committee were suggested by the congregation, we chose to
+  not conduct inquiries with ourselves as potential candidates until the end of
+  our first phase of inquires. When any member of the committee or their spouse
+  became a candidate, that committee member recused themselves from all
+  discussions and votes related to their candidacy.
+</p>
+
+<h3>
+  What was Pastor Mitch’s role on the Nominating Committee?
+</h3>
+<p>
+  According to Wellspring’s bylaws, the Sr. Pastor is an <i>ex officio</i>
+  member of the Nominating Committee. As we began our work this year, we sought
+  wisdom from Bill Wilson (from Center for Healthy Churches) and others
+  regarding the most constructive way for Pastor Mitch to participate. In light
+  of our recent conflicts and ongoing healing process, the committee and Pastor
+  Mitch agreed that he ought to play a more limited role this year. As we moved
+  closer to identifying nominees, Pastor Mitch shared important insights and
+  feedback, while never seeking to exert an undue influence on the outcome. As
+  the committee voted to decide on nominees, Pastor Mitch willingly recused
+  himself from this part of our process. We believe Pastor Mitch’s role was
+  constructive, balanced, and aligned with wisdom we received from Bill Wilson
+  and others.
+</p>
+
+<h3>
+  How do I nominate someone from the floor?
+</h3>
+<p>
+  According to Wellspring's bylaws, any active member may nominate another member
+  from the floor for elected offices. The individual being nominated must first
+  confirm that they are willing to be put forward as a nominee.
+</p>
+<p>
+  We want to make voting available for those members who are in-person as well as
+  members on the livestream, so we’ll be using an electronic voting system as we
+  have the past two years. Because of this, it will greatly help us to know in
+  advance of any floor nominations.
+</p>
+<p>
+  If you wish to nominate someone, please email their name and the role you’re
+  nominating them for to
+  <a href="mailto:nomcom@wellspringalliance.net">nomcom@wellspringalliance.net</a>
+  by June 10th (two days before the annual meeting).
+</p>
+<p>
+  Finally, there’s one role the Nominating Committee doesn’t formally nominate
+  candidates for, and that’s our next Nominating Committee! We will elect 3
+  members to this committee at the annual meeting, and our new Governing Board
+  will later appoint 3 additional members. If you know someone who is spiritually
+  mature, enjoys talking to people, and cares deeply about the leadership
+  needs of our church, please consider nominating them. As with other nominations,
+  we ask that you first confirm they are willing to serve in this role, and then
+  email their name and the role of Nom Com to
+  <a href="mailto:nomcom@wellspringalliance.net">nomcom@wellspringalliance.net</a>
+  by June 10th.
+</p>
+
+<br /><br /><br />
 
 <style>
   h3 {
